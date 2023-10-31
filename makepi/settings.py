bd55fdd5b66c5b8bb165dc89f4452e2ad99d252b
Jee <jeehyunkim0510@naver.com>
2023-10-31 19:51:59 +0900

    projectapp view etc and linked some article with accountapp datail and projectapp detail

--- makepi/settings.py
+++ makepi/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'profileapp',
     'articleapp',
     'commentapp',
+    'projectapp',
 ]
 
 MIDDLEWARE = [
@@ -132,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
---
+++ projectapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+from articleapp.views import ArticleCreateView, ArticleDetailView, ArticleUpdateView, ArticleDeleteView, ArticleListView
+from projectapp.views import ProjectListView, ProjectCreateView, ProjectDetailView
+
+app_name = "projectapp"
+
+
+
+
+urlpatterns = [
+    path('list/', ProjectListView.as_view(), name='list'),
+    path('create/', ProjectCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
+
+]
