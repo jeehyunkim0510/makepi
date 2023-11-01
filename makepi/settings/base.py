31a75c6c00f2cb907bc477df31001c19c6e98b12
Jee <jeehyunkim0510@naver.com>
2023-11-01 16:53:40 +0900

    created settings dir and local and deploy python file patched settings file to base

--- makepi/settings/base.py
+++ makepi/settings/base.py
@@ -15,13 +15,15 @@ from pathlib import Path
 import environ, os
 from django.urls import reverse_lazy
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
 env = environ.Env(
     DEBUG=(bool, False)
 )
 
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 #reading .env file
 environ.Env.read_env(
